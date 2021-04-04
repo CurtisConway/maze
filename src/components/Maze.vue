726ac19f223bffec8794f4c2d343b6233eb4d381
Curtis <curtis.conway14@gmail.com>
2021-04-03 18:19:38 -0700

    Add on screen controls

--- src/components/Maze.vue
+++ src/components/Maze.vue
@@ -34,13 +34,47 @@
           </tr>
         </table>
       </div>
-    </div>
 
-    <h1 v-if="success">YOU WIN!</h1>
-    <h1 v-if="failure">YOU LOSE!</h1>
-    <button v-if="success || failure" @click="resetGame">
-      {{ resetButtonText }}
-    </button>
+      <div class="row">
+        <div class="col" style="padding:30px 10px;">
+          <div class="row">
+            <button class="btn-sq" @click="keyboardActions.up">
+              <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path
+                  fill="currentColor"
+                  d="M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160
+            255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6
+            0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"></path>
+              </svg>
+            </button>
+          </div>
+          <div class="row">
+            <button class="btn-sq" @click="keyboardActions.left">
+              <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" class=""></path>
+              </svg>
+            </button>
+            <button class="btn-sq" @click="keyboardActions.down">
+              <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path fill="currentColor" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z" class=""></path></path>
+              </svg>
+            </button>
+            <button class="btn-sq" @click="keyboardActions.right">
+              <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" class=""></path></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div class="col">
+          <h1 v-if="success">YOU WIN!</h1>
+          <h1 v-if="failure">YOU LOSE!</h1>
+          <button v-if="success || failure" @click="resetGame">
+            {{ resetButtonText }}
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -70,6 +104,28 @@ export default {
     resetButtonText() {
       return this.success ? 'Next Level' : 'Try Again';
     },
+    gameOver() {
+      return this.success || this.failure;
+    },
+    keyboardActions() {
+      return {
+        up: () => {
+          if (this.currentPosition.top) this.movePosition(this.currentX, this.currentY - 1);
+        },
+        left: () => {
+          if (this.currentPosition.left) this.movePosition(this.currentX - 1, this.currentY);
+        },
+        down: () => {
+          if (this.currentPosition.bottom) this.movePosition(this.currentX, this.currentY + 1);
+        },
+        right: () => {
+          if (this.currentPosition.right) this.movePosition(this.currentX + 1, this.currentY);
+        },
+        reset: () => {
+          if (this.gameOver) this.resetGame();
+        },
+      };
+    },
   },
   mounted() {
     this.startGame();
@@ -95,34 +151,17 @@ export default {
       this.startGame();
     },
     keyboardHandler(event) {
-      const actions = {
-        up: () => {
-          if (this.currentPosition.top) this.movePosition(this.currentX, this.currentY - 1);
-        },
-        left: () => {
-          if (this.currentPosition.left) this.movePosition(this.currentX - 1, this.currentY);
-        },
-        down: () => {
-          if (this.currentPosition.bottom) this.movePosition(this.currentX, this.currentY + 1);
-        },
-        right: () => {
-          if (this.currentPosition.right) this.movePosition(this.currentX + 1, this.currentY);
-        },
-        reset: () => {
-          if (this.success || this.failure) this.resetGame();
-        },
-      };
       const handlers = {
-        w: actions.up,
-        a: actions.left,
-        s: actions.down,
-        d: actions.right,
-        ArrowUp: actions.up,
-        ArrowLeft: actions.left,
-        ArrowDown: actions.down,
-        ArrowRight: actions.right,
-        Enter: actions.reset,
-        ' ': actions.reset,
+        w: this.keyboardActions.up,
+        a: this.keyboardActions.left,
+        s: this.keyboardActions.down,
+        d: this.keyboardActions.right,
+        ArrowUp: this.keyboardActions.up,
+        ArrowLeft: this.keyboardActions.left,
+        ArrowDown: this.keyboardActions.down,
+        ArrowRight: this.keyboardActions.right,
+        Enter: this.keyboardActions.reset,
+        ' ': this.keyboardActions.reset,
       };
       if (handlers[event.key]) {
         event.preventDefault();
@@ -131,18 +170,20 @@ export default {
       }
     },
     movePosition(x, y) {
-      this.currentX = x;
-      this.currentY = y;
-      if (this.currentPosition.active) {
-        this.failure = true;
-        this.size = 3;
-      } else {
-        this.currentPosition.active = true;
-      }
+      if (!this.gameOver) {
+        this.currentX = x;
+        this.currentY = y;
+        if (this.currentPosition.active) {
+          this.failure = true;
+          this.size = 3;
+        } else {
+          this.currentPosition.active = true;
+        }
 
-      if (this.currentX === (this.maze.width - 1) && this.currentY === (this.maze.height - 1)) {
-        this.success = true;
-        this.size += 1;
+        if (this.currentX === (this.maze.width - 1) && this.currentY === (this.maze.height - 1)) {
+          this.success = true;
+          this.size += 1;
+        }
       }
     },
   },
@@ -189,7 +230,13 @@ button {
   border:none;
   border-radius:5px;
   cursor:pointer;
-  margin-top:20px;
+  margin:5px;
+
+  &.btn-sq {
+    width:50px;
+    height:50px;
+    padding:0;
+  }
 }
 td {
   border:thick double rgba(255, 255, 0, 0.95);
@@ -225,7 +272,28 @@ td {
     opacity:1;
   }
 }
-
+.row {
+  display:flex;
+  flex-direction:row;
+  justify-content:center;
+  align-items:flex-start;
+  flex-wrap:wrap;
+}
+.col {
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  flex:1 1 0;
+  padding:10px;
+  min-width:50%;
+  box-sizing:border-box;
+}
+@media screen and (max-width:640px) {
+  .col {
+    min-width:100%;
+  }
+}
 @keyframes fadeScaleIn {
   from {
     opacity:.75;
